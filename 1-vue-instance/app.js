fdc147a4e76b252a1b5701c8860f303bf22da2d1
emreavarisli <emreavarisli@gmail.com>
2023-09-24 23:52:20 +0300

    step-1

--- 1-vue-instance/app.js
+++ 1-vue-instance/app.js
@@ -10,6 +10,22 @@ const app = Vue.createApp({
                 alt : "okul-sitesi"
             },
             owner : "Pogaca",
+            coords: {
+                x: 0,
+                y: 0,
+            },
         };
+    },
+    methods : { //fonksiyonlar buraya yazılıyor
+        changeTitle(pTitle){
+            this.title = pTitle;
+        },
+        updateCoords(message, event){
+            //console.log(message, event.x, event.y);
+            this.coords = {
+                x: event.x,
+                y: event.y,
+            };
+        },
     },
 }).mount("#app");
