7aca66080f5386eef44a13287ee4c6e6c1141fa3
emreavarisli <emreavarisli@gmail.com>
2023-09-27 11:09:57 +0300

    step-3

--- 3-counter-app/app.js
+++ 3-counter-app/app.js
@@ -5,12 +5,23 @@ const app = Vue.createApp({
             counter2: 0,
         };
     },
-    methods: {
+    methods: {},
+    computed : {
         getCounterResult() {
+            console.log('Counter 1 çalıştı');
             return this.counter > 5 ? "BÜYÜK" : "KÜÇÜK";
         },
         getCounter2Result() {
+            console.log('Counter 1 çalıştı');
             return this.counter2 > 5 ? "BÜYÜK" : "KÜÇÜK";
+        },
+    },
+    watch : {
+        counter(newValue, oldValue) {
+            console.log("Counter", oldValue, "=>", newValue);
+        },
+        getCounterResult(newValue, oldValue) {
+            console.log("RESULT", oldValue, "=>", newValue);
         },
     },
     // methods : {
